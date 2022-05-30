bc69afa50b8b03a6cec455fdcdee97ae3e7b276a
ElinVS <elinvsvennberg@gmail.com>
2022-05-30 11:52:04 +0100

    Elin is bleeding

---
+++ coding/client/src/components/QuizForm.js
@@ -0,0 +1,18 @@
+import React from "react"
+
+
+const QuizForm = (()=>{
+
+    return (
+        <>
+            <h1>Quiz Form</h1>
+        </>
+    )
+
+})
+
+
+
+
+
+export default QuizForm
--- coding/client/src/containers/Quiz.js
+++ coding/client/src/containers/Quiz.js
@@ -1,4 +1,33 @@
+import React, { useState } from "react"
+
+
+//A static quiz. We will have 10 questions
+// Each option will be a radio - button
+//when the user selects the right answer the score should increament by 1 point? or only show score on finalScore?
+
+//When the quiz is completed a final score should be displayed. if final score is less than < 4 also show a set of links or a set of explanation to aid in the students learning
+//does final score then need to be its own component for the if elseif else statement?
+//option 2 : on submit return  
+
+
+//How will the quiz data be set up? An array of quiz objects? 
+// Needs a isCorrect: true or false
+//map out the array and return new array?
+//map out the choices or the questions?
+
+
 const Quiz = (()=>{
+
+    //Properties needed: Score / finalScore / currentQuestion / 
+    
+    const [score, setScore] = useState(0)
+    const [finalScore, setFinalScore] = useState(false)
+    const [currentQuestion, setCurrentQuestion] = useState(0)
+
+
+const Quiz = (()=>{
+
+
     return(
         <>
             <h2>I am the Quiz container</h2>
