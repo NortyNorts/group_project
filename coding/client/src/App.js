57a93c4955506c565d634560f3e374ee2d1d8f5f
Andrew Smith <andrew_c_smith@hotmail.com>
2022-06-01 09:45:08 +0100

    router update2

--- coding/client/src/App.js
+++ coding/client/src/App.js
@@ -5,10 +5,9 @@ import Stickies from './containers/Stickies';
 import RubberDucking from './containers/RubberDucking';
 import Anecdotes from './components/Anecdotes';
 import Links from './components/Links';
-import StickiesTwo from './containers/StickiesTwo';
-import QuizTwo from './containers/QuizTwo';
 import NavBar from './components/NavBar';
 import Home from './components/Home';
+import './css/stickies.css'
 
 function App() {
   return (
@@ -21,10 +20,8 @@ function App() {
             <Route exact path="/"> <Home /></Route>
             
             <Route exact path="/quiz"><Quiz /></Route>
-            <Route exact path="/quiztwo"><QuizTwo /></Route>
             
             <Route path="/stickies"><Stickies /></Route>
-            <Route path="/stickiestwo"><StickiesTwo /></Route>
             
             <Route path="/rubberducking"><RubberDucking /></Route>
 
--- coding/client/src/components/NavBar.js
+++ coding/client/src/components/NavBar.js
@@ -1,15 +1,17 @@
 import { Link } from "react-router-dom"
 import Header from "./Header"
+import '../css/stickies.css'
 
 const NavBar =() => {
     return (
         <>
-        <nav className = "navbar">
+        <nav className = "navbar" id="navbar">
             {/* <Header/> */}
         <h1>ERIK</h1>
         <div className="links">
             <Link to ='/'>  Home  - </Link>
-            <a href='/quiztwo'>  Quiz  - </a>
+            
+            <a href='/quiz'>  Quiz  - </a>
             <Link to='/rubberducking'>  Rubber Duck  - </Link>
             <a href='/stickies'>  Pin Board  - </a>
             <Link to='/anecdotes'>  Anecdotes  - </Link>
